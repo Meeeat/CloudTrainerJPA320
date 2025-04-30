bc3ddd23358f32f28358b37220d8ec4d52ae557e
Siberia <angroov@gmail.com>
2025-04-30 21:21:55 +0900

    ADD exceptions handler

---
+++ src/main/java/com/cloudtrainerjpa320/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.cloudtrainerjpa320.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private int errorCode;
+    private String errorMessage;
+}
